7a33ea73880d6e659cc5b7a9cd3cf503d558c2b9
Nisarg Patel <23csd11@svitvasad.ac.in>
2024-10-08 15:14:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { div } from 'framer-motion/client'
+
+const App = () => {
+
+  return (
+    <div>
+      3D Developer Portfolio 
+    </div>
+  )
+}
+
+export default App
